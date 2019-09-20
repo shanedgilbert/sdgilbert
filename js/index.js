f66c6e3474e04408cca941975a0606dc68effc7e
Shane Gilbert <shanedgilbert@gmail.com>
2019-09-20 23:30:58 +0800

    added thumbnails

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 window.onload = function() {
   // JavaScript for scroll to top button
   const scrollToTopButton = document.getElementById('scroll-to-top');
+  const downButton = document.getElementById('scroll-to-bottom');
   const scrollFunc = () => {
     // Get the current scroll value
     let y = window.scrollY;
@@ -8,8 +9,10 @@ window.onload = function() {
     // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
     if (y > 500) {
       scrollToTopButton.className = "scroll-top show";
+      downButton.className = "down-arrow hide";
     } else {
       scrollToTopButton.className = "scroll-top hide";
+      downButton.className = "down-arrow show";
     }
   };
   window.addEventListener("scroll", scrollFunc);
